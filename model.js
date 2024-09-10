0c239f0ec00d1be3a2ae57c4f6864fdb5e932df3
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-09-10 17:04:51 +0200

    first push

---
+++ model.js
@@ -0,0 +1,42 @@
+
+
+const model = {
+    app: {
+        userName: '',
+        password: '',
+        page: 'UserPage',
+    },
+    input:{
+        transfer: '',
+        kid: '',
+        accountNumber: '',
+    },
+    data: {
+       
+        users: [
+            {
+                name: '',
+                userName: '',
+                password: '',
+                MainBalance: '',
+                SavingsAccount: '',
+                Billingaccount: '',
+            }
+        ],
+
+        admins: [
+            {
+                name: '',
+                userName: '',
+                password: '',
+                adminLevel: '',
+                page: 'adminPage'
+            }
+        ],
+
+        clock: '',
+        news: [],
+        
+
+    },
+}
